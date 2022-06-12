b6e0e8a30087a3034dc17166a30efc78a4637740
Liina-Maria Bakhoff <liina.bakhoff@gmail.com>
2022-06-13 00:34:35 +0300

    Added mad-lib replacement variables to story

---
+++ app.js
@@ -0,0 +1,24 @@
+"use strict";
+
+const originalStory = "Once upon a time, there was a very naughty shepherd called John. He would take his sheep for grazing, then climb up a tree, and shout for help, claiming that a wolf was attacking his sheep. '/n' Every time this happened, the farmers and other men would come to rescue him and his flock. But once they saw he was lying, they would leave angrily. Then, the boy would laugh. This went on for a long time, but one day, a wolf really came and attacked his sheep. But this time nobody heard him or offered to help him because they did not believe him anymore."
+let userInputs = [];
+
+let newStory = "Once upon a time, there was a very ${userInputs[1]} ${userInputs[2]} called ${userInputs[3]}. He would take his ${userInputs[4]} for grazing, then climb up a ${userInputs[5]}, and shout for help, claiming that a ${userInputs[6]} was attacking his ${userInputs[4]}. '/n' Every time this happened, the farmers and other men would come to rescue ${userInputs[3]} and his flock. But once they saw he was lying, they would leave angrily. Then, the boy would laugh. This went on for a long time, but one day, a ${userInputs[6]} really came and attacked his ${userInputs[4]}. But this time nobody heard him or offered to help him because they did not believe him anymore."
+
+
+
+console.log('newStory: ' + newStory);
+
+
+
+
+
+
+
+
+
+
+
+/* Once upon a time, there was a very naughty shepherd boy. He would take his sheep for grazing, then climb up a tree, and shout for help, claiming that a wolf was attacking his sheep.
+
+Every time this happened, the farmers and other men would come to rescue him and his flock. But once they saw he was lying, they would leave angrily. Then, the boy would laugh. This went on for a long time, but one day, a wolf really came and attacked his sheep. But this time nobody heard him or offered to help him because they did not believe him anymore. */
